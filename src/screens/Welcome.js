719bcfa31fff575f1bd002b88162287476fe0398
paisul <paisul.paree@elegantmedia.lk>
2024-08-04 12:22:06 +0530

    welcome screen added

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
-function Welcome() {
+
+function Welcome({ navigation }) {
   return (
-    <View>
-        <Text>Welcome</Text>
+    <View style={styles.container}>
+       <Image source={require('../../assets/salad_2515263.png')} style={styles.imageStyle} />
+      <Text style={styles.headline}>Yummie Wheels</Text>
+      <Text style={{ color: "black", fontWeight: "900", marginBottom: 20,}}>From Kitchen to Your Doorstep</Text>
+      <Button title='start' color={"red"} onPress={() => navigation.navigate('TabNavigator')}/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      imageStyle: {
+    width: "50%",
+    height: undefined,
+    aspectRatio: 1,
+  },
+  headline: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "red",
+    fontStyle: "italic",
+    marginTop: 20,
+  },
+})
 
 export default Welcome;
