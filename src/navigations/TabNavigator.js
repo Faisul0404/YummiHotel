1c8a7d7a8e825b24dc137ead9bd102e6d16a3a26
paisul <paisul.paree@elegantmedia.lk>
2024-08-11 09:44:50 +0530

    added icons to xcode and Fixed image not showing issue

--- src/navigations/TabNavigator.js
+++ src/navigations/TabNavigator.js
@@ -1,12 +1,69 @@
 import React from 'react'
 import { Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home';
+import Products from '../screens/Products';
+import About from '../screens/About';
+import Notifications from '../screens/Notifications';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import  AntDesign  from 'react-native-vector-icons/AntDesign';
 
-function TabNavigator() {
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigator() {
   return (
-    <View>
-        <Text>Tap Navigator</Text>
-    </View>
+        <Tab.Navigator screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: true,
+          tabBarStyle: {
+            height: 90,
+            borderRadius: 20,
+            // marginVertical: 20,
+            paddingVertical: 20,
+            position: "absolute",
+          },
+        }}>
+          <Tab.Screen 
+          name='Home' 
+          component={Home} 
+          options={{
+            tabBarIcon: ({focused}) => {
+              return (
+                <Ionicons name={ focused ? "home" : "home-outline"} size={30} color={"black"} />
+              )
+            },
+          }} 
+          />
+          <Tab.Screen 
+            name='Products'
+            component={Products}
+            options={{
+              tabBarIcon: ({ focused }) => {
+                return (
+                  <Ionicons name={focused ? 'bag-check' : 'bag-check-outline'}size={30} color={"black"}/>
+                )
+              }
+            }}
+          />
+          <Tab.Screen 
+            name='Notifications'
+            component={Notifications}
+            options={{
+              tabBarIcon: ({ focused }) => {
+                return <Ionicons name={ focused ? 'notifications' : 'notifications-outline'} size={30} color={'black'} />
+              }
+            }}
+          />
+          <Tab.Screen 
+            name='About'
+            component={About}
+            options={{
+              tabBarIcon: ({ focused }) => {
+                return <Ionicons name={ focused ? 'person' : 'person-outline'} size={30} color={'black'} />
+              }
+            }}
+          />
+        </Tab.Navigator>
   )
 }
-
-export default TabNavigator;
