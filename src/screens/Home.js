3bd214ab8e09d02141a38672a7c13a38efdf6e74
paisul <paisul.paree@elegantmedia.lk>
2024-08-11 14:58:43 +0530

    content added

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
 export default function Home({ navigation }) {
@@ -12,11 +12,19 @@ export default function Home({ navigation }) {
         <TouchableOpacity 
          onPressIn={()=> setIconColor('red')}
          onPressOut={()=> setIconColor('black')}
-         onPress={()=> navigation.navigate('Welcome')} >
+         onPress={()=> navigation.navigate('Welcome')} 
+         style={{margin: 10}} >
           <AntDesign name = 'arrowleft' size={30} color={iconColor} />
         </TouchableOpacity>
+        </View>
+        <View style={style.container}>
+          <Image source={require('../../assets/pexels-xmtnguyen-699953.jpg')} style={style.img} />
+          <Text style={style.headerColor}>Home</Text>
+          <Text>Test One</Text>
 
-
+          <View style={style.moreButton} >
+     <Text onPress={() => navigation.navigate('Detail')}  >more details</Text>
+     </View>
         </View>
     </SafeAreaView>
   )
@@ -25,4 +33,27 @@ export default function Home({ navigation }) {
 
 const style = StyleSheet.create({
 
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  img: {
+    width: "50%",
+    height: undefined,
+    aspectRatio: 1,
+  }, 
+  headerColor: {
+    fontWeight: "800",
+        fontSize: 30,
+        color: 'blue',
+  },
+  moreButton: {
+    borderRadius: 10,
+    color: "red",
+    fontSize: 18,
+    backgroundColor: "cyan",
+    padding: 10,
+    paddingTop: 10,
+  },
 })
--- src/screens/Products.js
+++ src/screens/Products.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native'
 
-function Products() {
+export default function Products() {
   return (
-   <View>
-        <Text>Products</Text>
-   </View>
+   <SafeAreaView style={{flex:1, backgroundColor: '#F2E8C6'}}>
+          <View style={style.container} >
+            <Text>Products</Text>
+          </View>
+   </SafeAreaView>
   )
 }
 
-export default Products
+const style = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+
+})
