b698b35a3fc426b2ec0e5eac5c28de9fe8227af0
paisul <paisul.paree@elegantmedia.lk>
2024-08-18 19:10:31 +0530

    added back button

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { React, useState } from 'react'
+import { Text, View, SafeAreaView, TouchableOpacity } from 'react-native'
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
-function Details() {
+export default function Details({ navigation }) {
+
+  const [iconColor, setIconColor] = useState("black");
+
   return (
-    <View>
-        <Text>Details</Text>
-    </View>
+    <SafeAreaView style={{flex:1, backgroundColor: '#F2E8C6'}}> 
+        <View >
+        <TouchableOpacity 
+         onPressIn={()=> setIconColor('red')}
+         onPressOut={()=> setIconColor('black')}
+         onPress={()=> navigation.navigate('Home')} 
+         style={{margin: 10}} >
+          <AntDesign name = 'arrowleft' size={30} color={iconColor} />
+        </TouchableOpacity>
+        </View>
+      </SafeAreaView>
   )
 }
-
-export default Details
