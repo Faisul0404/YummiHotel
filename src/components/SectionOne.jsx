7402a28fe3999c35a9f3d437380ea744064dd472
paisul <paisul.paree@elegantmedia.lk>
2024-08-23 08:22:12 +0530

    javascript array mapping implemeneted

--- src/components/SectionOne.jsx
+++ src/components/SectionOne.jsx
@@ -1,63 +1,78 @@
-import {StyleSheet, Text, View, Image, TouchableOpacity, Alert} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 import React from 'react';
 import {ScrollView} from 'react-native-gesture-handler';
+
+const SectionOneData = [
+  {
+    id: '01',
+    headline: 'Product 01',
+    tagline: 'About the product 01',
+    img: require('../../assets/hamburger.jpg'),
+  },
+  {
+    id: '02',
+    headline: 'Product 02',
+    tagline: 'About the product 02',
+    img: require('../../assets/prown_gravy.jpg'),
+  },
+  {
+    id: '03',
+    headline: 'Product 03',
+    tagline: 'About the product 03',
+    img: require('../../assets/hamburger.jpg'),
+  },
+  {
+    id: '04',
+    headline: 'Product 04',
+    tagline: 'About the product 04',
+    img: require('../../assets/thai-seafood.jpg'),
+  },
+  {
+    id: '05',
+    headline: 'Product 05',
+    tagline: 'About the product 05',
+    img: require('../../assets/rice-noodles.jpg'),
+  },
+  {
+    id: '06',
+    headline: 'Product 06',
+    tagline: 'About the product 06',
+    img: require('../../assets/hamburger.jpg'),
+  },
+];
 
 export default function SectionOne() {
   return (
     <View style={styles.container}>
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-      <TouchableOpacity onPress={() => alert("alert 01")}>
-        <View style={styles.imgContainer}>
-          <Image
-            source={require('../../assets/prown_gravy.jpg')}
-            style={styles.imgStyle}
-          />
-          <Text style={styles.headline}>Headline</Text>
-          <Text>About the product - Tagline</Text>
-        </View>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => alert("alert 02")}>
-        <View style={styles.imgContainer}>
-          <Image
-            source={require('../../assets/rice-noodles.jpg')}
-            style={styles.imgStyle}
-          />
-          <Text style={styles.headline}>Headline</Text>
-          <Text>About the product - Tagline</Text>
-        </View>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => alert("alert 03")}>
-        <View style={styles.imgContainer}>
-          <Image
-            source={require(`../../assets/hamburger.jpg`)}
-            style={styles.imgStyle}
-          />
-          <Text style={styles.headline}>Headline</Text>
-          <Text>About the product - Tagline</Text>
-        </View>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => alert("alert 04")}>
-        <View style={styles.imgContainer}>
-          <Image
-            source={require('../../assets/thai-seafood.jpg')}
-            style={styles.imgStyle}
-          />
-          <Text style={styles.headline}>Headline</Text>
-          <Text>About the product - Tagline</Text>
-        </View>
-        </TouchableOpacity>
+        {SectionOneData.map(item => (
+          <TouchableOpacity key={item.id} onPress={() => alert(item.id)}>
+            <View style={styles.imgContainer}>
+              <Image source={item.img} style={styles.imgStyle} />
+              <Text style={styles.headline}>{item.headline}</Text>
+              <Text>{item.tagline}</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
       </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        margin: 20,
-    },
-    imgContainer: {
-        marginRight: 10,
-    },
+  container: {
+    margin: 20,
+  },
+  imgContainer: {
+    marginRight: 10,
+  },
   imgStyle: {
     width: 200,
     height: undefined,
