4e0d7f720415f892c444d8289116f95c9ce36051
paisul <paisul.paree@elegantmedia.lk>
2024-08-24 08:49:52 +0530

    modified content

--- src/components/SectionTwo.jsx
+++ src/components/SectionTwo.jsx
@@ -1,10 +1,4 @@
-import {
-  Image,
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-} from 'react-native';
+import {Image, StyleSheet, Text, View, FlatList} from 'react-native';
 import React from 'react';
 // import { ScrollView } from 'react-native-gesture-handler';
 
@@ -14,57 +8,66 @@ const items = [
     title: 'Item 01',
     description: 'Information about item',
     image: require('../../assets/thai-seafood.jpg'),
+    price: 'Rs. 1200.00',
   },
   {
     id: '2',
     title: 'Item 02',
     description: 'Information about item',
     image: require('../../assets/hamburger.jpg'),
+    price: 'Rs. 1500.00',
   },
   {
     id: '3',
     title: 'Item 03',
     description: 'Information about item',
     image: require('../../assets/prown_gravy.jpg'),
+    price: 'Rs. 2200.00',
   },
   {
     id: '4',
     title: 'Item 04',
     description: 'Information about item',
     image: require('../../assets/thai-seafood.jpg'),
+    price: 'Rs. 3999.00',
   },
   {
     id: '5',
     title: 'Item 05',
     description: 'Information about item',
     image: require('../../assets/rice-noodles.jpg'),
+    price: 'Rs. 200.00',
   },
   {
     id: '6',
     title: 'Item 06',
     description: 'Information about item',
     image: require('../../assets/salad_2515263.png'),
+    price: 'Rs. 1200.00',
   },
   {
     id: '7',
     title: 'Item 07',
     description: 'Information about item',
     image: require('../../assets/thai-seafood.jpg'),
+    price: 'Rs. 2340.00',
   },
   {
     id: '8',
     title: 'Item 08',
     description: 'Information about item',
     image: require('../../assets/prown_gravy.jpg'),
+    price: 'Rs. 1200.00',
   },
 ];
 
-const ItemCard = ({title, description, image}) => (
+const ItemCard = ({title, description, image, price}) => (
   <View style={styles.cardContainer}>
     <Image source={image} style={styles.imgContainer} />
     <View>
       <Text style={styles.title}>{title}</Text>
       <Text style={styles.description}>{description}</Text>
+      <Text style={styles.price}>{price}</Text>
     </View>
   </View>
 );
@@ -73,7 +76,7 @@ export default function SectionTwo() {
   return (
     <>
       <View>
-        <Text style={styles.sectionHeader}> Section 02 </Text>
+        <Text style={styles.sectionHeader}>Featured Products</Text>
       </View>
       <View>
         <FlatList
@@ -83,6 +86,7 @@ export default function SectionTwo() {
               title={item.title}
               description={item.description}
               image={item.image}
+              price={item.price}
             />
           )}
           keyExtractor={item => item.id}
@@ -104,19 +108,34 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingBottom: 20, // Adds padding at the bottom to ensure the last item is fully visible
     alignItems: 'flex-start', // Centers the list items horizontally
+    marginTop: 20,
   },
   cardContainer: {
     marginBottom: 20,
     flexDirection: 'row',
     gap: 20,
     justifyContent: 'flex-start',
+    paddingLeft: 10,
+    paddingVertical: 10,
+    backgroundColor: '#F2E8C6',
+    paddingRight: 100,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.39,
+    shadowRadius: 8.3,
+
+    elevation: 13,
   },
   imgContainer: {
     width: 100,
     height: undefined,
     aspectRatio: 1,
     borderRadius: 10,
-    marginBottom: 10,
+    // marginBottom: 10,
   },
   title: {
     fontSize: 18,
@@ -125,13 +144,19 @@ const styles = StyleSheet.create({
   },
   description: {
     fontSize: 14,
-    color: 'gray',
+    color: 'black',
     textAlign: 'center',
   },
   sectionHeader: {
-    fontSize: 24,
-    marginBottom: 20,
+    fontSize: 26,
+    // marginBottom: 20,
+    // fontWeight: 'bold',
+    // textAlign: 'center',
+    marginLeft: 20,
+  },
+  price: {
+    fontSize: 14,
+    marginTop: 10,
     fontWeight: 'bold',
-    textAlign: 'center',
   },
 });
