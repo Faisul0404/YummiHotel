36168c8e6997cfd6cd4b9566ebc7bddb1ad54857
paisul <paisul.paree@elegantmedia.lk>
2024-08-25 20:36:37 +0530

    modified and added background image

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {StyleSheet, Text, View, Image, Button} from 'react-native';
+import {StyleSheet, Text, View, Image, Button, ImageBackground, TouchableOpacity} from 'react-native';
 
 function Welcome({navigation}) {
   return (
+    <ImageBackground style={{flex:1, }} source={require('../../assets/backgroundImage/bg01.jpg')} >
     <View style={styles.container}>
       <Image
         source={require('../../assets/salad_2515263.png')}
@@ -12,19 +13,25 @@ function Welcome({navigation}) {
       <Text style={{color: 'black', fontWeight: '900', marginBottom: 20}}>
         From Kitchen to Your Doorstep
       </Text>
-      <Button
+      <View style={styles.button} >
+      {/* <Button
         title="start"
-        color={'red'}
+        color={'white'}
         onPress={() => navigation.navigate('TabNavigator')}
-      />
+      /> */}
+      <TouchableOpacity onPress={() => navigation.navigate('TabNavigator')} style={styles.button} >
+        <Text  style={styles.buttonText}>Start</Text>
+      </TouchableOpacity>
+      </View>
     </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    // backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -39,6 +46,18 @@ const styles = StyleSheet.create({
     color: 'red',
     fontStyle: 'italic',
     marginTop: 20,
+  },
+  button: {
+    backgroundColor: 'red',
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    borderRadius: 5,
+    borderRadius: 20,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: '300',
+    color: 'white',
   },
 });
 
